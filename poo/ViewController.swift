06394517f80ef9c72d21d6db9f575127a70aba7f
MacInnovacion1 <MacInnovacion1@imac-de-macinnovacion2.local>
2018-10-17 11:55:34 -0500

    Initial Commit

---
+++ poo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  poo
+//
+//  Created by MacInnovacion1 on 17/10/18.
+//  Copyright © 2018 CENTRON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
